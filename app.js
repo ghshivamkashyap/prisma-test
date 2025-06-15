60f60366e35b89b5be0292b3f3576ead8fc5756c
SHIVAM KASHYAP <lootforecast@gmail.com>
2025-06-15 12:51:05 +0530

    Add health check endpoint and initialize Prisma client

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ const PORT = process.env.PORT || 4000;
 const authRoutes = require("./routes/auth");
 const jobRoutes = require("./routes/job");
 const connectDB = require("./config/db");
+const { PrismaClient } = require("./generated/prisma");
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(express.json());
 connectDB();
@@ -19,6 +21,13 @@ app.use(
 
 app.get("/", (req, res) => {
   res.status(200).json({ success: true, message: "Welcome to the API!" });
+});
+
+app.get("/health", async (req, res) => {
+  const data = await prisma.jobs.findMany();
+  return res
+    .status(200)
+    .json({ success: true, message: "Server is healthy!", data });
 });
 
 app.use("/api/auth", authRoutes);
